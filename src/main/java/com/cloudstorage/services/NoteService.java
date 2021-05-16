cb67ab32b5c330d56a1e050867a3e452343dbaf6
Vladislav <57865785+oO777Oo@users.noreply.github.com>
2021-05-16 23:11:50 +0200

    First view of Project
    First View of Project

---
+++ src/main/java/com/cloudstorage/services/NoteService.java
@@ -0,0 +1,39 @@
+package com.cloudstorage.services;
+
+import com.cloudstorage.mapper.NoteMapper;
+import com.cloudstorage.mapper.UserMapper;
+import com.cloudstorage.model.Note;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+    private final UserMapper userMapper;
+    private final NoteMapper noteMapper;
+
+    public NoteService(UserMapper userMapper, NoteMapper noteMapper) {
+        this.userMapper = userMapper;
+        this.noteMapper = noteMapper;
+    }
+
+    public void addNote(String title, String description, String userName) {
+        Integer userId = userMapper.getUser(userName).getUserId();
+        Note note = new Note(0, title, description, userId);
+        noteMapper.insert(note);
+    }
+
+    public Note[] getNoteListings(Integer userId) {
+        return noteMapper.getNotesForUser(userId);
+    }
+
+    public Note getNote(Integer noteId) {
+        return noteMapper.getNote(noteId);
+    }
+
+    public void deleteNote(Integer noteId) {
+        noteMapper.deleteNote(noteId);
+    }
+
+    public void updateNote(Integer noteId, String title, String description) {
+        noteMapper.updateNote(noteId, title, description);
+    }
+}
